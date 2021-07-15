ProtonAOSP/android_vendor_proton

vendorsetup: Override hostname to avoid leaking info

vendorsetup.sh
@@ -1,5 +1,8 @@
 # Shebang is intentionally missing - do not run as a script
 
+# Override hostname to make builds more reproducible and avoid leaking info
+export BUILD_HOSTNAME=protonaosp-build
+
 # ABI compatibility checks fail for several reasons:
 #   - The update to Clang 12 causes some changes, but no breakage has been
 #     observed in practice.

maldini03/vendor_banana

vendorsetup.sh
@@ -1,5 +1,8 @@
 # Shebang is intentionally missing - do not run as a script
 
+# Override hostname to make builds more reproducible and avoid leaking info
+export BUILD_HOSTNAME=bananadroid-build
+
 # ABI compatibility checks fail for several reasons:
 #   - The update to Clang 12 causes some changes, but no breakage has been
 #     observed in practice.